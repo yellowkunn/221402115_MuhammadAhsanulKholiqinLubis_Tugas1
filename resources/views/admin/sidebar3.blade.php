12b971fe501d193d6e7b8f01cbcb668277910b11
unknown <ahsanlubid308@gmail.com>
2023-04-07 08:36:29 +0700

    Tugas 1 Prak PWL

---
+++ resources/views/admin/sidebar3.blade.php
@@ -0,0 +1,71 @@
+@extends('admin/layout')
+
+@section('content')
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">
+
+                        
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/style3.css">
+</head>
+<body>
+    <div class="containerr">
+
+    <div class="image1">
+        <img src="/img/itachi/main_foto.png" alt="" height="546px" width="546px">
+    </div>
+
+    <div class="teksbox">
+    “Seberat apapun beban masalah <br> yang kamu hadapi saat ini, <br> percayalah bahwa semua <br> itu tidak pernah melebihi <br> batas kemampuan kamu.”
+    </div>
+
+    
+    <div class="bg1">
+        <img src="/img/itachi/bg1.png" alt="" width="350" height="350">
+    </div>
+    <div class="bg3">
+        <img src="/img/itachi/bg3.jpg" alt="" width="561" height="469">
+    </div>
+
+    <div class="teksbox2">
+        <img src="/img/itachi/bgchild3.1.png" alt="">
+    </div>
+
+    <div class="tumbal"></div>
+    <div class="persegi">
+    <p>
+    I'm sorry I won't be able to watch you grow up. <br> But know that I'll always be with you. <br> You don't have to forgive me. But no matter <br> what you decide to do from here on out, <br> know this... I will love you always    </p>    </div>
+    <div class="kosong">
+        <div class="last">
+        <img src="/img/itachi/bg2.png" alt="" width="280" height="331">
+        </div>
+    </div>
+
+    <div class="itachi">
+        <img src="/img/itachi/itachi.png" alt="" width="305" height="566">
+    </div>
+
+        </div>
+
+
+</body>
+</html>
+
+
+</div>
+
+</div>
+<!-- /.container-fluid -->
+@endsection('content')
