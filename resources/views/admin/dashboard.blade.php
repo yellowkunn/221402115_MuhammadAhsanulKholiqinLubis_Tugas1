6c09de5df2a416594471aecdc5133f3813a961bb
ahsan <123297976+yellowkunn@users.noreply.github.com>
2023-04-09 09:32:29 +0700

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,7 +6,10 @@
 
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+                        <h1 class="h3 mb-0 text-gray-800">
+                    DASHBOARD <br>
+                    Ubah tampilan layar menjadi 90% untuk melihat secara maksimal
+                    </h1>
                     </div>
 
                 </div>
